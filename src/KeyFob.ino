a99741d15cd76e3d5fabd6193d06942ed4814e07
Tuet <lutz.lisseck@gmx.de>
2024-10-28 23:52:43 +0100

    Trigger now works, but slow

--- src/KeyFob.ino
+++ src/KeyFob.ino
@@ -1,24 +1,110 @@
 #include <ESP8266WiFi.h>
 #include <QuickEspNow.h>
 #include <elapsedMillis.h>
+#include <Crypto.h>
+#include <Ed25519.h>
+#include <MyCreds.h> // Contains DOOR_PRIKEY, DOOR_PUBKEY, DOOR_NAME (not in GIT...)
+
+#ifndef DOOR_PRIKEY
+#define DOOR_PRIKEY "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff"
+#define DOOR_PUBKEY "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff"
+#define DOOR_NAME   "Name for Key UTF8"
+#endif
 
 #define ESPNOW_CHANNEL 13
-#define ESPNOW_PREFIX "D00"
-
-// Commands: ? will answer via ESPNOW-Broadcast
 
 #define DEST_ADDR ESPNOW_BROADCAST_ADDRESS 
 
 // Debug prints on Serial activ if defined
-#define ESPNOW_TESTMODE 
+//#define ESPNOW_TESTMODE 
 
 // Send something every 5s
 //#define ESPNOW_AUTOSEND
 
-const char espNowPrefix[] = ESPNOW_PREFIX;
-int espNowPrefixLen;
-volatile bool doAnswer = false;
+volatile int mainState = 0;
 
+uint8_t door_prikey[32];
+uint8_t door_pubkey[32];
+uint8_t door_name[32];
+uint8_t lastChallenge[8];
+
+uint8_t hex1(const char c) {
+  uint8_t val = 255;
+  if((c >= 'A') && (c <= 'F')) val = c - 'A' + 10;
+  if((c >= 'a') && (c <= 'f')) val = c - 'a' + 10;
+  if((c >= '0') && (c <= '9')) val = c - '0';
+  return val;
+}
+
+size_t hex2bytes(const char *hexString, uint8_t *byteArray, size_t byteArrayLen) {
+  size_t byteCount = 0;
+
+  while(byteCount < byteArrayLen) {
+    uint8_t h = 0;
+    uint8_t l = 0;
+    char c;
+    while((h = hex1(c = *hexString++)) == 255) {
+      if(c == 0) break;
+    }
+    if(c == 0) break;
+    while((l = hex1(c = *hexString++)) == 255) {
+      if(c == 0) break;
+    }
+    if(c == 0) break;    
+    *byteArray++ = h*16+l;
+    byteCount++;
+  }
+
+  return byteCount;
+}
+
+bool loadKey(const char *priKey, const char *pubKey, const char *name) {
+  size_t len = 0;
+  len = hex2bytes(priKey, door_prikey, 32);
+  if(len != 32) return false;
+  len = hex2bytes(pubKey, door_pubkey, 32);
+  if(len != 32) return false;  
+  memset(door_name, 0, 32);
+  strlcpy((char *)door_name, name, 31);
+  uint8_t temp_pubkey[32];
+  Ed25519::derivePublicKey(temp_pubkey, door_prikey);
+  if(memcmp(temp_pubkey, door_pubkey, 32) != 0) return false;
+  return true;
+}  
+
+/**
+ * Protocol Tx:
+ * Off Len
+ *   0   4  Preemble "D00r"
+ *   4  64  Signature
+ *  68  32  Public Key
+ * 100  32  Name (Cleartext UTF8, 0 filled)
+ * 132   8  Challenge respone
+ * 140   1  Command (might be longer than 1 if needed)   
+ * 
+ */
+bool txCommand(const char *cmd) {
+  uint8_t msg[4+64+32+32+8+8];
+  int cmdlen = strlen(cmd);
+  if(cmdlen > 8) cmdlen = 8;
+  msg[0] = 'D'; msg[1] = '0'; msg[2] = '0'; msg[3] = 'r';
+  memcpy(&msg[68], door_pubkey, 32);
+  memcpy(&msg[100], door_name, 32);
+  memcpy(&msg[132], lastChallenge, 8);
+  memcpy(&msg[140], cmd, cmdlen+1);
+  Ed25519::sign(&msg[4], door_prikey, door_pubkey, &msg[68], 32+32+8+cmdlen+1);
+  return(quickEspNow.send(DEST_ADDR, msg, 4+64+32+32+8+cmdlen+1) == 0);
+}
+
+/**
+ * Protocol Rx:
+ * Off Len
+ *   0   4  Preemble "D00a" (for answer)
+ *   4  32  Public Key from Rx
+ *  36   8  New challenge
+ *  44   1  Answer (might be longer than 1 if needed)
+ * 
+ */ 
 void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi, bool broadcast) {
   #ifdef ESPNOW_TESTMODE
     Serial.print ("Received: ");
@@ -26,28 +112,54 @@ void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi
     Serial.printf ("RSSI: %d dBm\n", rssi);
     Serial.printf ("From: " MACSTR "\n", MAC2STR (address));
     Serial.printf ("%s\n", broadcast ? "Broadcast" : "Unicast");
-    Serial.printf ("Prefixlen %d\n", espNowPrefixLen);
   #endif
-  if(len > espNowPrefixLen) {
-    if(strncmp((const char *)data, espNowPrefix, espNowPrefixLen) == 0) {
-      data[len] = 0; // termination of string
-      Serial1.print((const char *)&data[espNowPrefixLen]);
-      Serial.printf("Data:%s\n", (const char *)&data[espNowPrefixLen]);
-      if(data[espNowPrefixLen] == '?') doAnswer = true;
+  // Check if it is valid Door protocol
+  if(len >= (4+32+8+1)) {
+    if(((data[0] == 'D') && (data[1] == '0') && (data[2] == '0') && (data[3] == 'a'))) {
+      if(memcmp(&data[4], door_pubkey, 32) == 0) {
+        char cmd = data[44];
+        memcpy(lastChallenge, &data[36], 8);
+
+        switch (mainState)
+        {
+          case 1: // challenge requested, expect now 'c'
+            if(cmd == 'c') mainState++; else mainState = 100;
+            break;
+
+          case 3: // trigger requested, expect now 't'
+            if(cmd == 't') mainState++; else mainState = 100;
+            break;
+          
+          default:
+            break;
+        }
+        Serial.printf("RSSI: %d dBm, RxCmd: %.*s\n", rssi, len-44, &data[44]);
+      }  
     }
   }
+
 }
 
+uint32_t timestamp = 0;
+
 void setup() {
-  espNowPrefixLen = strlen(espNowPrefix);
   Serial.begin(74880);   //  GPIO1 (TX) and GPIO3 (RX)
+  #ifdef ESPNOW_TESTMODE
   Serial.setDebugOutput(true);
+  #endif
   pinMode(2, OUTPUT);
-  //pinMode(3, INPUT_PULLUP);
-  Serial1.begin(115200); //  GPIO2 TX1 (output only, connected to LED)
+  pinMode(0, INPUT_PULLUP);
+  //Serial1.begin(115200); //  GPIO2 TX1 (output only, connected to LED)
   //WiFi.disconnect();
 	//ESP.eraseConfig();
 	//delay(3000);
+  Serial.printf("MS: %d\n", (int)millis());
+  if(loadKey(DOOR_PRIKEY, DOOR_PUBKEY, DOOR_NAME) == false) {
+    Serial.println("loadKey failed!");
+  } else {
+    Serial.println("\n\nloadKey ok.");
+  }  
+
   WiFi.mode(WIFI_STA);
   //wifi_promiscuous_enable(true); // this kills broadcast receive capability!
   //wifi_set_channel(ESPNOW_CHANNEL);
@@ -62,12 +174,54 @@ void setup() {
     Serial.println(system_get_sdk_version());
   #endif
   quickEspNow.onDataRcvd(dataReceived);
+
+  Serial.printf("MS0: %d (Trig via reset)\n", (int)(millis() - timestamp));
+  if(txCommand("c")) mainState = 1;
+  Serial.printf("MS1: %d\n", (int)(millis() - timestamp));
 }
 
 char msg[256];
+bool butTrig = false;
 void loop() {
   static elapsedMillis bcTx = 0;
   static int cnt = 0;
+
+  if(mainState == 2) {
+    Serial.printf("MS2: %d\n", (int)(millis() - timestamp));
+    if(txCommand("t")) mainState = 3;
+    Serial.printf("MS3: %d\n", (int)(millis() - timestamp));
+  }
+
+  if(mainState == 4) {
+    Serial.printf("MS4: %d\n", (int)(millis() - timestamp));
+    Serial.println("Trigger done!\n");
+    mainState++;
+  }  
+
+  if((digitalRead(0) == LOW) || (butTrig)) {
+    butTrig = false;
+    timestamp = millis();
+    if(txCommand("c")) mainState = 1;
+    Serial.printf("MS1: %d (Trig via but)\n", (int)(millis() - timestamp));
+    delay(10);
+    while(digitalRead(0) == LOW) {}
+    delay(10);
+  }  
+
+  // put your main code here, to run repeatedly:
+  if(Serial.available()) {
+    char b = Serial.read();
+    if(b == 't') {
+      butTrig = true;
+    } else {
+      sprintf(msg,"%c", b);
+      if (txCommand(msg)) {
+          Serial.println (">>>>>>>>>> Message sent");
+      } else {
+          Serial.println (">>>>>>>>>> Message not sent");
+      }
+    }
+  }  
 
   // Testbroadcast every 5s
   if(bcTx > 5000) {
@@ -83,29 +237,6 @@ void loop() {
     #endif
   }
 
-  if(doAnswer) {
-    doAnswer = false;
-    sprintf(msg,"KeyFob V1.0, Uptime: %lu", millis());
-
-    if (!quickEspNow.send (DEST_ADDR, (uint8_t*)msg, strlen(msg))) {
-        Serial.println (">>>>>>>>>> Answer Message sent");
-    } else {
-        Serial.println (">>>>>>>>>> Answer Message not sent");
-    }
-  }
-
-  // put your main code here, to run repeatedly:
-  if(Serial.available()) {
-    char b = Serial.read();
-
-    sprintf(msg,"D00g%c", b);
-
-    if (!quickEspNow.send (DEST_ADDR, (uint8_t*)msg, strlen(msg))) {
-        Serial.println (">>>>>>>>>> Message sent");
-    } else {
-        Serial.println (">>>>>>>>>> Message not sent");
-    }
-  }  
   delay(1);
 }
 
