c3b121de97f2b134021ba610da2bbcc6c58ca257
Tuet <lutz.lisseck@gmx.de>
2024-12-20 19:24:28 +0100

    Added power safe options and higher data rate.

--- src/KeyFob.ino
+++ src/KeyFob.ino
@@ -1,9 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <QuickEspNow.h>
+#include <espnow.h>
 #include <elapsedMillis.h>
 #include <Crypto.h>
 #include <Ed25519.h>
 #include <MyCreds.h> // Contains DOOR_PRIKEY, DOOR_PUBKEY, DOOR_NAME (not in GIT...)
+#include <EEPROM.h>
+
+#include "user_interface.h"
+#include "include/WiFiState.h"
 
 #ifndef DOOR_PRIKEY
 #define DOOR_PRIKEY "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff"
@@ -12,8 +17,8 @@
 #endif
 
 #define ESPNOW_CHANNEL 13
-
 #define DEST_ADDR ESPNOW_BROADCAST_ADDRESS 
+#define ESPNOW_TX_POWER 10.0 /* dBm */
 
 // Debug prints on Serial activ if defined
 //#define ESPNOW_TESTMODE 
@@ -27,6 +32,20 @@ uint8_t door_prikey[32];
 uint8_t door_pubkey[32];
 uint8_t door_name[32];
 uint8_t lastChallenge[8];
+
+uint8_t rand_data[32];
+
+ADC_MODE(ADC_VCC); 
+uint16_t vcc = 0;
+
+void blinkLED(int cnt, int ms) {
+  for(int i=0; i<cnt; i++) {
+    digitalWrite(2, LOW); // LED on
+    delay(ms);
+    digitalWrite(2, HIGH);
+    delay(ms);
+  }
+} 
 
 uint8_t hex1(const char c) {
   uint8_t val = 255;
@@ -67,8 +86,8 @@ bool loadKey(const char *priKey, const char *pubKey, const char *name) {
   memset(door_name, 0, 32);
   strlcpy((char *)door_name, name, 31);
   uint8_t temp_pubkey[32];
-  Ed25519::derivePublicKey(temp_pubkey, door_prikey);
-  if(memcmp(temp_pubkey, door_pubkey, 32) != 0) return false;
+  //Ed25519::derivePublicKey(temp_pubkey, door_prikey);
+  //if(memcmp(temp_pubkey, door_pubkey, 32) != 0) return false;
   return true;
 }  
 
@@ -93,6 +112,7 @@ bool txCommand(const char *cmd) {
   memcpy(&msg[132], lastChallenge, 8);
   memcpy(&msg[140], cmd, cmdlen+1);
   Ed25519::sign(&msg[4], door_prikey, door_pubkey, &msg[68], 32+32+8+cmdlen+1);
+//  WiFi.forceSleepWake();
   return(quickEspNow.send(DEST_ADDR, msg, 4+64+32+32+8+cmdlen+1) == 0);
 }
 
@@ -141,14 +161,28 @@ void dataReceived (uint8_t* address, uint8_t* data, uint8_t len, signed int rssi
 }
 
 uint32_t timestamp = 0;
+elapsedMillis activeTime;
 
 void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(0, INPUT_PULLUP);  
+  digitalWrite(2, HIGH);
   Serial.begin(74880);   //  GPIO1 (TX) and GPIO3 (RX)
   #ifdef ESPNOW_TESTMODE
   Serial.setDebugOutput(true);
   #endif
-  pinMode(2, OUTPUT);
-  pinMode(0, INPUT_PULLUP);
+
+  EEPROM.begin(512);
+  //WiFi.forceSleepBegin(1000000UL);
+  vcc = system_get_vdd33();
+
+  //Serial.printf("SDK version: %s \n", system_get_sdk_version());
+  Serial.printf("VCC %d\r\n", vcc);
+
+  int ret = os_get_random((unsigned char *)rand_data, 32);
+  Serial.printf("Rand ret %d, %02x %02x %02x %02x\r\n", ret, 
+    (int)rand_data[0], (int)rand_data[1], (int)rand_data[30], (int)rand_data[31]);
+
   //Serial1.begin(115200); //  GPIO2 TX1 (output only, connected to LED)
   //WiFi.disconnect();
 	//ESP.eraseConfig();
@@ -156,6 +190,7 @@ void setup() {
   Serial.printf("MS: %d\n", (int)millis());
   if(loadKey(DOOR_PRIKEY, DOOR_PUBKEY, DOOR_NAME) == false) {
     Serial.println("loadKey failed!");
+    blinkLED(5, 200);
   } else {
     Serial.println("\n\nloadKey ok.");
   }  
@@ -165,7 +200,11 @@ void setup() {
   //wifi_set_channel(ESPNOW_CHANNEL);
   //wifi_promiscuous_enable(false); 
   WiFi.disconnect (false);
-  
+
+  quickEspNow.onDataRcvd(dataReceived);
+  // wifi_set_phy_mode();
+  // esp_wifi_config_espnow_rate(WIFI_IF_STA, WIFI_PHY_RATE_11M_L /* WIFI_PHY_RATE_LORA_250K */ );
+
   quickEspNow.begin(ESPNOW_CHANNEL); // 
   #ifdef ESPNOW_TESTMODE
     Serial.printf ("Connected to %s in channel %d\n", WiFi.SSID ().c_str (), WiFi.channel ());
@@ -173,18 +212,28 @@ void setup() {
     Serial.printf ("MAC address: %s\n", WiFi.macAddress ().c_str ());
     Serial.println(system_get_sdk_version());
   #endif
-  quickEspNow.onDataRcvd(dataReceived);
+  
+  system_phy_set_max_tpw(40); // 10mW/10dBm (0.25dBm x X, 0..82)
+  wifi_set_phy_mode(PHY_MODE_11N);
+  
+  //WiFi.setOutputPower(ESPNOW_TX_POWER);
+  wifi_set_user_fixed_rate(FIXED_RATE_MASK_ALL, PHY_RATE_54);
 
-  Serial.printf("MS0: %d (Trig via reset)\n", (int)(millis() - timestamp));
+  activeTime = 0;
+
+  //WiFi.forceSleepBegin(1000000UL);
+  Serial.printf("MS0: %d (Trig via reset) %d\n", (int)(millis() - timestamp), wifi_get_phy_mode());
   if(txCommand("c")) mainState = 1;
   Serial.printf("MS1: %d\n", (int)(millis() - timestamp));
+  wifi_set_sleep_type(MODEM_SLEEP_T);
+  os_delay_us(100);
+  wifi_set_sleep_type(NONE_SLEEP_T);
 }
 
 char msg[256];
 bool butTrig = false;
+
 void loop() {
-  static elapsedMillis bcTx = 0;
-  static int cnt = 0;
 
   if(mainState == 2) {
     Serial.printf("MS2: %d\n", (int)(millis() - timestamp));
@@ -195,12 +244,13 @@ void loop() {
   if(mainState == 4) {
     Serial.printf("MS4: %d\n", (int)(millis() - timestamp));
     Serial.println("Trigger done!\n");
-    mainState++;
+    blinkLED(1, 200);
+    mainState = 10;
   }  
 
   if((digitalRead(0) == LOW) || (butTrig)) {
     butTrig = false;
-    timestamp = millis();
+    timestamp = millis(); activeTime = 0;
     if(txCommand("c")) mainState = 1;
     Serial.printf("MS1: %d (Trig via but)\n", (int)(millis() - timestamp));
     delay(10);
@@ -222,19 +272,17 @@ void loop() {
       }
     }
   }  
-
-  // Testbroadcast every 5s
-  if(bcTx > 5000) {
-    bcTx = 0;
-    sprintf(msg,"D00g%d", cnt++);
-
-    #ifdef ESPNOW_AUTOSEND
-    if (!quickEspNow.send (DEST_ADDR, (uint8_t*)msg, strlen(msg))) {
-        Serial.println (">>>>>>>>>> Message sent");
+  
+  if(activeTime > 3000) {
+    activeTime = 0;
+    if((mainState != 10) && (mainState != 101)) {
+      Serial.println("Timeout!");
+      blinkLED(3, 100);
+      mainState = 101;
     } else {
-        Serial.println (">>>>>>>>>> Message not sent");
+      ESP.deepSleep(0);
     }
-    #endif
+    //WiFi.forceSleepBegin(1000000UL);
   }
 
   delay(1);
